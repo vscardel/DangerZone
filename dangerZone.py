a4f45aa1e483264bf653b7f653e9171faf23a5dc
vscardel <vscardel@gmail.com>
2019-10-03 19:59:51 -0300

    inicializando a grid com predadores e presas

--- dangerZone.py
+++ dangerZone.py
@@ -29,49 +29,67 @@ def check_colision(pos,grid):
 	else:
 		return True
 
+#creates an animal that is not out of bound or colides with another animal
+def create_safe_animal(obj,grid):
+
+	#keeps on the loop until it produces an animal that does not goes 
+	#out of bounds or colides with another animal
+	while True:
+
+		pos_x = np.random.randint(num_columns)
+		pos_y = np.random.randint(num_rows)
+		obj.position = (pos_x,pos_y)
+		obj.constroi_corpo()
+
+		flag_keep_on_loop = False
+
+		for bp in obj.body:
+
+			out_of_bounds = check_boundaries(bp,num_rows,num_columns)
+
+			if not out_of_bounds:
+				colision = check_colision(bp,grid)
+
+			if out_of_bounds or colision:
+				flag_keep_on_loop = True
+				break
+
+		if not flag_keep_on_loop:
+			break
+
 def populate_grid(grid,num_prey,num_predators):
 	#receives imput from user
 	num_prey,num_predators = int(sys.argv[1]),int(sys.argv[2])
 
-	#construct the predators
+	#construct the animals
 	predators = []
+	preys = []
+
 	for i in range(num_predators):
 
-		obj = Predator()
-		obj.id = i
+		animal = Predator()
+		animal.id = i
+		create_safe_animal(animal,grid)
 
-		#keeps on the loop until it produces a animal that does not goes 
-		#out of bounds or colides with another animal
-		while True:
-
-			pos_x = np.random.randint(num_columns)
-			pos_y = np.random.randint(num_rows)
-			obj.position = (pos_x,pos_y)
-			obj.constroi_corpo()
-
-			flag_keep_on_loop = False
-
-			for bp in obj.body:
-
-				out_of_bounds = check_boundaries(bp,num_rows,num_columns)
-				print(out_of_bounds)
-
-				if not out_of_bounds:
-					colision = check_colision(bp,grid)
-
-				if out_of_bounds or colision:
-					flag_keep_on_loop = True
-					break
-
-			if not flag_keep_on_loop:
-				break
-
-		for bp in obj.body:
+		for bp in animal.body:
 			pos_x,pos_y = bp[0],bp[1]
 			grid[pos_y][pos_x] = 2
 
-		predators.append(obj)
-	return predators
+		predators.append(animal)
+
+	for i in range(num_prey):
+
+		animal = Prey()
+		animal.id = i
+		create_safe_animal(animal,grid)
+
+		for bp in animal.body:
+			pos_x,pos_y = bp[0],bp[1]
+			grid[pos_y][pos_x] = 1
+
+		preys.append(animal)
+
+	return predators,preys
 
 
 class Predator():
@@ -89,6 +107,25 @@ class Predator():
 		up_right = (self.position[0]+1,self.position[1]+1)
 		up_left = (self.position[0]+1,self.position[1]-1)
 		self.body = [self.position,up_left,up_right,down_left,down_right]
+
+class Prey():
+
+	def __init__(self):
+		self.id = None
+		#position of the center of the predator on the grid
+		self.position = None
+		#colection of cells that composes the animal
+		self.body = None
+
+	def constroi_corpo(self):
+		positions = np.empty([5])
+		down = (self.position[0],self.position[1]+1)
+		up = (self.position[0],self.position[1]-1)
+		right = (self.position[0]+1,self.position[1])
+		left = (self.position[0]-1,self.position[1])
+		self.body = [self.position,down,up,left,right]
+
+
 
 #inherits from Window class
 class DangerZone(arcade.Window):
@@ -119,13 +156,15 @@ class DangerZone(arcade.Window):
 		num_preys,num_predators = sys.argv[1],sys.argv[2]
 
 		#call function that populates the grid with preys and predators
-		populate_grid(self.grid,num_preys,num_predators)
+		predators,preys = populate_grid(self.grid,num_preys,num_predators)
 
 		#transverse grid to check state of each position
 		for i in range(num_rows):
 			for j in range(num_columns):
 				if self.grid[i][j] == 2:
 					color = arcade.color.BLUE
+				elif self.grid[i][j] == 1:
+					color = arcade.color.GREEN
 				else:
 					color = arcade.color.WHITE
 				
